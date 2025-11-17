651e3719783f0bc8910d093ed1027a09af39f359
Vitner4 <101582707+Vitner4@users.noreply.github.com>
2025-11-17 17:05:11 +0300

    Обновление лицензии ПО

--- web/script.js
+++ web/script.js
@@ -1,8 +1,9 @@
 
 // Отображение текущего года в подвале сайта
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('current-year').textContent = new Date().getFullYear();
+    document.getElementById('current-year').textContent = 2025;
+    // document.getElementById('current-year').textContent = new Date().getFullYear();
 });
 
 // Ссылка на GitHub OtakuBoard
-document.getElementById('github-link').href = 'https://github.com/Omiraiix/OtakuBoard';
+document.getElementById('github-link').href = 'https://github.com/Vitner4/OtakuBoard';
